86886982c35601962a7cc847a5e46b1a63a3c766
Vanessa Bell <vanessa.bell14@gmail.com>
2017-04-17 18:55:37 -0700

    added content, fixed datetime on events, added lightbox for event details

--- kiosk/views.py
+++ kiosk/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse
 from .models import Faqs
+from datetime import datetime
 
 # Create your views here.
 def IndexView(request):
@@ -17,18 +18,31 @@ def EventsView(request):
 	url = 'https://events.hackerdojo.com/events.json'
 	resp = requests.get(url=url)
 	data = resp.json()
-	# TODO change dates from strings to dates
+	today = datetime.today()
+	for event in data:
+		event['start_time'] = datetime.strptime(event['start_time'], '%Y-%m-%dT%H:%M:%S')
+		# if event['start_time'] < datetime.today():
+		# 	print('this was yesterday',event)
+		# 	data.remove(event)
+		# else:
+		event['end_time'] = datetime.strptime(event['end_time'], '%Y-%m-%dT%H:%M:%S')
+		event['day'] = event['start_time'].strftime("%A, %B %d, %Y")
+		event['start'] = event['start_time'].strftime("%I:%M %p")
+		event['end'] = event['end_time'].strftime("%I:%M %p")
 	# TODO don't show past events
-	return render(request, 'kiosk/events.html',{'data':data})
+	return render(request, 'kiosk/events.html',{'data':data,'today':today})
 
 def BuildingMapView(request):
     return render(request, 'kiosk/building_map.html')
 
-def PricingView(request):
-    return render(request, 'kiosk/pricing.html')
+def MembershipView(request):
+    return render(request, 'kiosk/membership.html')
 
 def AboutUsView(request):
     return render(request, 'kiosk/about_us.html')
+
+def MapView(request):
+    return render(request, 'kiosk/map.html')
 
 
 
