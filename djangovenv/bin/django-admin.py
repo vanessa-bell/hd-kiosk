1f885f68fc27967871f0044486edd579b6940617
Denys Korovets <denys_kr@yahoo.com>
2017-04-23 22:10:13 -0700

    added gitignore, removed venv and .idea

--- djangovenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/vanessabell/Desktop/hackerdojo/djangovenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- djangovenv/lib/python3.6/site-packages/schedule/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from schedule.models.calendars import Calendar, CalendarRelation  # noqa
-from schedule.models.events import *  # noqa
-from schedule.models.rules import *  # noqa
-
-from schedule.signals import *  # noqa
